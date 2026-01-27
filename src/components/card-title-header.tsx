b13f109f3db518dc12f9e5ed9303be379d46a891
killian-dv <killian.david@etudiant.univ-reims.fr>
2026-01-27 22:16:36 +0100

    Enhance CardTitleHeader styling

--- src/components/card-title-header.tsx
+++ src/components/card-title-header.tsx
@@ -1,7 +1,7 @@
 export const CardTitleHeader = () => {
   return (
     <div className="flex">
-      <h1>Renewable Energy Generation</h1>
+      <h1 className="text-xl font-semibold">Renewable Energy Generation</h1>
     </div>
   );
 };
