23c5ae72cc50f13869af7340a4e06d4e16e6b05e
killian-dv <killian.david@etudiant.univ-reims.fr>
2026-01-27 22:16:44 +0100

    Update CardIconHeader to enhance icon styling with size and opacity

--- src/components/card-icon-header.tsx
+++ src/components/card-icon-header.tsx
@@ -7,8 +7,12 @@ import { HugeiconsIcon } from "@hugeicons/react";
 export const CardIconHeader = () => {
   return (
     <div className="flex flex-row justify-between items-center">
-      <HugeiconsIcon icon={BatteryEcoChargingIcon} />
-      <HugeiconsIcon icon={MoreHorizontalIcon} />
+      <HugeiconsIcon
+        icon={BatteryEcoChargingIcon}
+        size={32}
+        className="opacity-80"
+      />
+      <HugeiconsIcon icon={MoreHorizontalIcon} size={32} />
     </div>
   );
 };
